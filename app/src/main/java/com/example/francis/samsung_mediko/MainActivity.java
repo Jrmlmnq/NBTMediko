2dfe89682f9942a9d299b6c0a7a4b6e68d001538
Mark <mark_francis_benedicto@dlsu.edu.ph>
2018-07-04 17:49:58 +0800

    Added with firebase shit

--- app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
+++ app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.francis.samsung_mediko;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-
-        Button signUpBtn = (Button) findViewById(R.id.signupBtn);
-
-        signUpBtn.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getBaseContext(), RegisterActivity.class));
-            }
-        });
     }
 }
