237d5ee1d7c4cbed34c3cc7b33c753e97b95c856
Jrmlmnq <jerome_lominoque@dlsu.edu.ph>
2018-07-04 17:26:51 +0800

    Edited MainActivity and Added RegisterActivity

--- app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
+++ app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.francis.samsung_mediko;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
+
+        Button signUpBtn = (Button) findViewById(R.id.signupBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), RegisterActivity.class));
+            }
+        });
     }
 }
