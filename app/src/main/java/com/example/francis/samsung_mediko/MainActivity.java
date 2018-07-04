18878a6a1150182eb7eb9cd4e8609d34f42ac927
Jrmlmnq <jerome_lominoque@dlsu.edu.ph>
2018-07-04 19:04:01 +0800

    Pushy pushy v2

--- app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
+++ app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.francis.samsung_mediko;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_activity);
+
+        Button signUpBtn = (Button) findViewById(R.id.signupBtn);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/francis/samsung_mediko/RegisterActivity.java
+++ app/src/main/java/com/example/francis/samsung_mediko/RegisterActivity.java
@@ -2,11 +2,23 @@ package com.example.francis.samsung_mediko;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
+        Button backBtn = (Button) findViewById(R.id.registerBackButton);
+
+        backBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
