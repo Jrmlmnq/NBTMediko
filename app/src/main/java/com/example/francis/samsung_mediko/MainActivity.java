cffb032572e20ba4e59e91ef9f8a855dd20a9a69
Mark <mark_francis_benedicto@dlsu.edu.ph>
2018-07-04 18:28:31 +0800

    Pushy pushy

--- app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
+++ app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.francis.samsung_mediko;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_activity);
     }
 }
