d11e00b39ed318c66103a43f2006845cd177ce81
Mark <mark_francis_benedicto@dlsu.edu.ph>
2018-07-05 18:56:55 +0800

    Added the Login Function

--- app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
+++ app/src/main/java/com/example/francis/samsung_mediko/MainActivity.java
@@ -1,29 +1,66 @@
 package com.example.francis.samsung_mediko;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LOGIN";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//      requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_activity);
 
+        mAuth = FirebaseAuth.getInstance();
         Button signUpBtn = (Button) findViewById(R.id.signupBtn);
+        Button logInBtn = (Button) findViewById(R.id.loginBtn);
 
         signUpBtn.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getBaseContext(), RegisterActivity.class));
+            }
+        });
+
+        logInBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String email = (String) ((TextView) findViewById(R.id.ecpEdit)).getText().toString();
+                String password = (String) ((TextView) findViewById(R.id.pEdit)).getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
